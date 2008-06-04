6c59f09fc2535c7d1f59c2b1d472321029d280b2
Sergey Nebolsin <nebolsin@gmail.com>
2008-06-04 04:43:51 +0000

    Corrected job template to use spaces instead of tabs.
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-quartz/trunk@41472 832c1c66-4827-0410-8465-ccb17913cd09

--- src/templates/artifacts/Job.groovy
+++ src/templates/artifacts/Job.groovy
@@ -1,7 +1,7 @@
-class @artifact.name@ {
-	def timeout = 5000l     // execute job once in 5 seconds
+class @artifact.name @{
+    def timeout = 5000l // execute job once in 5 seconds
 
     def execute() {
-	    // execute task
-	}
+        // execute task
+    }
 }

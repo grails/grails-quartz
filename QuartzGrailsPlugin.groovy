8c516273281e0d26c8cfd4d2bcd4d177b220df1f
Sergey Nebolsin <nebolsin@gmail.com>
2010-02-27 14:03:53 +0300

    Updated required Grails version to '1.1 > *'

--- QuartzGrailsPlugin.groovy
+++ QuartzGrailsPlugin.groovy
@@ -39,6 +39,8 @@ import org.quartz.JobDataMap
 class QuartzGrailsPlugin {
 
     def version = "0.4.2"
+    def grailsVersion = "1.1 > *"
+    
     def author = "Sergey Nebolsin"
     def authorEmail = "nebolsin@gmail.com"
     def title = "This plugin adds Quartz job scheduling features to Grails application."

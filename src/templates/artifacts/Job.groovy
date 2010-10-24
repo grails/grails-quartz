20bb71e069a3f5c67d781bc87b05de485acba2c8
Sergey Nebolsin <nebolsin@gmail.com>
2010-10-25 02:01:42 +0400

    Updates job template for create-job script to follow new triggers configuration convention

--- src/templates/artifacts/Job.groovy
+++ src/templates/artifacts/Job.groovy
@@ -1,8 +1,11 @@
 @artifact.package@
+
 class @artifact.name@ {
-    def timeout = 5000l // execute job once in 5 seconds
+    static triggers = {
+      simple repeatInterval: 5000l // execute job once in 5 seconds
+    }
 
     def execute() {
-        // execute ‰job
+        // execute job
     }
 }

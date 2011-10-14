3ba7974d2e4daefe8ef434bfc5b5d0fa599a86b4
Chris Fraser <cfraser@superflav.com>
2011-10-14 14:42:42 -0700

    exluding slf4j-api because the version it pulls doesn't play nicely w/grails 1.3.7

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,7 +5,7 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.project.dependency.resolution = {
     inherits "global"
     log "warn"
-    
+
     repositories {
         grailsHome()
         grailsCentral()
@@ -15,7 +15,7 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         compile('org.hibernate:hibernate-core:3.3.1.GA', "org.quartz-scheduler:quartz:1.8.4") {
-            excludes 'ehcache', 'xml-apis', 'commons-logging'
+            excludes 'ehcache', 'xml-apis', 'commons-logging', 'slf4j-api'
         }
     }
 }

1238fbc55053e0c53288be115ec9297aaba99752
Ryan Vanderwerf <rvanderwerf@gmail.com>
2012-12-28 15:12:17 -0600

    RC4

--- QuartzGrailsPlugin.groovy
+++ QuartzGrailsPlugin.groovy
@@ -40,7 +40,7 @@ import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
  */
 class QuartzGrailsPlugin {
 
-    def version = "1.0-RC3"
+    def version = "1.0-RC4"
     def grailsVersion = "1.2 > *"
 
     def author = "Sergey Nebolsin, Graeme Rocher, Ryan Vanderwerf"

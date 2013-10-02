4587845434dfba85154c390d38a442c9d469e92b
Burt Beckwith <burt@burtbeckwith.com>
2013-10-02 12:17:38 -0400

    fixed release plugin issue

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,4 @@
-grails.project.source.level = 1.6
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target"
 
 grails.project.dependency.resolution = {
     inherits "global"
@@ -16,6 +13,12 @@ grails.project.dependency.resolution = {
     dependencies {
         compile("org.quartz-scheduler:quartz:2.2.0") {
             excludes 'slf4j-api', 'c3p0'
+        }
+    }
+
+    plugins {
+        build ':release:3.0.1', ':rest-client-builder:1.0.3', {
+            export = false
         }
     }
 }

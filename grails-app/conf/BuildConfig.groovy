bb25546938d9d9d3e207ff5d8d926c7619382b9d
Vitalii Samolovskikh <kefir@perm.ru>
2013-11-04 15:13:36 +0600

    Resolve merge conflict.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,10 +10,6 @@ grails.project.dependency.resolution = {
         mavenCentral()
     }
 
-    plugins {
-        build ":release:3.0.1", { export = false }
-    }
-
     dependencies {
         compile("org.quartz-scheduler:quartz:2.2.0") {
             excludes 'slf4j-api', 'c3p0'

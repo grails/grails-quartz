9c5f08c8ec1930eef9c9a581e2c2a258939a153d
Jeff Scott Brown <jbrown@pivotal.io>
2015-02-25 21:29:20 -0600

    add some overloaded schedule(...) methods

--- src/main/groovy/grails/plugins/quartz/QuartzJob.groovy
+++ src/main/groovy/grails/plugins/quartz/QuartzJob.groovy
@@ -24,20 +24,42 @@ import org.quartz.JobKey
 import org.quartz.Scheduler
 import org.quartz.SimpleTrigger
 import org.quartz.Trigger
+import org.quartz.spi.MutableTrigger
+import org.springframework.util.Assert
 
 trait QuartzJob implements WebAttributes {
     private static Scheduler internalScheduler
     private static GrailsJobClass internalJobArtefact
 
     static triggerNow(Map params = null) {
-        GrailsJobClass jobArtefact = internalGetJobArtefact()
+        def jobArtefact = internalGetJobArtefact()
         def scheduler = internalGetScheduler()
         scheduler.triggerJob(new JobKey(this.getName(), jobArtefact.group), params ? new JobDataMap(params) : null)
     }
 
     static schedule(Long repeatInterval, Integer repeatCount = SimpleTrigger.REPEAT_INDEFINITELY, Map params = null) {
-        GrailsJobClass jobArtefact = internalGetJobArtefact()
+        def jobArtefact = internalGetJobArtefact()
         internalScheduleTrigger(TriggerUtils.buildSimpleTrigger(this.getName(), jobArtefact.group, repeatInterval, repeatCount), params)
+    }
+
+    static schedule(Date scheduleDate, Map params = null) {
+        def jobArtefact = internalGetJobArtefact()
+        internalScheduleTrigger(TriggerUtils.buildDateTrigger(this.getName(), jobArtefact.group, scheduleDate), params)
+    }
+
+    static schedule(Trigger trigger, Map params = null) {
+        def jobArtefact = internalGetJobArtefact()
+        def jobKey = new JobKey(this.getName(), jobArtefact.group)
+        Assert.isTrue trigger.jobKey == jobKey || (trigger instanceof MutableTrigger),
+                "The trigger job key is not equal to the job key or the trigger is immutable"
+
+        trigger.jobKey = jobKey
+
+        if (params) {
+            trigger.jobDataMap.putAll(params)
+        }
+        def scheduler = internalGetScheduler()
+        scheduler.scheduleJob(trigger)
     }
 
     private static internalScheduleTrigger(Trigger trigger, Map params = null) {
